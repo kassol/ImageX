9e92856210a722db4424898fc347308735825913
kassol <kassol.zx@gmail.com>
2013-11-13 10:38:02 +0800

    rebuild the solution

--- ImageX/ImageX_p.c
+++ ImageX/ImageX_p.c
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Wed Oct 30 15:03:47 2013
+/* at Wed Nov 13 10:33:46 2013
  */
 /* Compiler settings for ImageX.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 7.00.0555 
